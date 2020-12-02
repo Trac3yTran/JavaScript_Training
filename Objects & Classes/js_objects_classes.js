469cdd294a4f06799d98b2fbc7598f21a2a75be0
Trac3yTran <tran.tracey90@gmail.com>
2020-12-02 17:30:06 +1100

    prototype property

--- Objects & Classes/js_objects_classes.js
+++ Objects & Classes/js_objects_classes.js
@@ -21,4 +21,33 @@ function Person(name, age) {
     this.awesome = true;
 }
 
-let instructor = new Person("Tracey")
+let person1 = new Person("Tracey", 30)
+console.log(person1)
+
+// constructor functions
+// object constructors can also be define member functions(analogous to defining a method within the class in Ruby)
+
+function Hero(name, level) {
+    this.name = name;
+    this.level = level;
+    this.shout = function () {
+        return `My real name is ${this.name}`;
+
+    }
+
+}
+
+let superman = new Hero("Clarke", 10000);
+console.log(superman.shout())
+
+//console.log same as puts output information only to the console
+//return output the value 
+
+//Prototype property
+// Can be used to add a default property after object creation
+
+let student = new Person("Andrew", 35);
+Person.prototype.role = "student";
+
+// the prototype is a template object that is shared between all instances of the same constructor in this case, Person.
+// Similar to a superclass in Ruby, except this isnt inheritance. 
