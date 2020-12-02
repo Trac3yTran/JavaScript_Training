72dbb3570c06b86f82e562044fdb1b54ebcbbc9e
Trac3yTran <tran.tracey90@gmail.com>
2020-12-02 19:01:36 +1100

    class inheritance

--- Classes & Mixins/js_classes_mixins.js
+++ Classes & Mixins/js_classes_mixins.js
@@ -24,3 +24,55 @@ console.log(rect.area())
 // Static properties
 // Static properties are accessed with the class name instead of an instance variable. Use them to define properties that are common for al instances of a class
 
+// Class inheritaince
+// We can use the extends keyword to inherit from another class. We can use the super keyword to execute the superclass function. 
+
+class Animal {
+    constructor(name) {
+        this.name = name
+    }
+    speak() {
+        return `My name is ${this.name}!`
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, breed) {
+        super(name)
+        this.breed = breed
+    }
+    speak() {
+        return `Woof! ${super.speak()} I'm a ${this.breed}! `
+    }
+}
+
+let animal = new Dog("Ted", "Border Collie")
+console.log(animal)
+
+//Object.assign
+
+// Used to add or modify properties for an object instance. This is a form of composition we are combining the properties of several objects into one object 
+
+const student = {
+    name: "Tracey",
+    course: "none"
+
+}
+const FTstudent = {
+    course: "FastTrack",
+    start: "October"
+}
+
+const CAStudent = {
+    languages: ["ruby", "javascript"],
+    speak: function () {
+        console.log("I can do it!")
+    }
+}
+
+let tracey = Object.assign(student, FTstudent, CAStudent)
+console.log(tracey)
+tracey.speak()
+
+// Mixins
+
