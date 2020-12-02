00bb4ddd0b8d85f0f6256c0bbf088fd253ec6ed1
Trac3yTran <tran.tracey90@gmail.com>
2020-12-02 18:45:51 +1100

    static properties

--- Classes & Mixins/js_classes_mixins.js
+++ Classes & Mixins/js_classes_mixins.js
@@ -17,3 +17,10 @@ console.log(rect.area())
 
 // constructor is equivalent to initialize in Ruby
 // This refere to the object itself and is equivalent to using @ in Ruby
+
+// JavaScript is object oriented but not class-based
+// From MDN docs "JavaScript classes, introduced in ECMAScript 2015 are primarily syntactical sugar over JavaScript's existing protoype-based inheritance. The class syntax does not introduce a new object -oriented inheritance model of JavaScript"
+
+// Static properties
+// Static properties are accessed with the class name instead of an instance variable. Use them to define properties that are common for al instances of a class
+
