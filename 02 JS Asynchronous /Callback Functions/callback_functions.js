a7529670dda9c443f9e82af7454cac28841c3d17
Trac3yTran <tran.tracey90@gmail.com>
2020-12-10 16:14:02 +1100

    callback maths functions

--- 02 JS Asynchronous /Callback Functions/callback_functions.js
+++ 02 JS Asynchronous /Callback Functions/callback_functions.js
@@ -21,3 +21,18 @@ processInput("Alex", greet);
 processInput('daniel', excitedGreet);
 
 // It allows us to write thats more modular and reuseable
+
+//math functions
+
+function add(a,b) {
+    console.log(a + b)
+}
+
+function multiply(x, y) {
+    console.log(x * y);
+}
+
+function doMath(num1, num2, cb) {
+    console.log("processing");
+    cb(num1, num2);
+}
